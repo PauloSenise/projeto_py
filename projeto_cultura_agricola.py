792ee1af4db3b8bd96f1fbebfc16c1f61183722a
PauloSenise <paulo.senise@hotmail.com>
2025-04-19 14:54:10 -0300

    Update projeto_cultura_agricola.py

--- projeto_cultura_agricola.py
+++ projeto_cultura_agricola.py
@@ -1,4 +1,4 @@
-# Aluno: Paulo Henrique Senise - RM: 565781 - Turma:  1TIAOA - 2025/1
+# Aluno: Paulo Henrique - Turma:  1TIAOA - 2025/1
 # Biblioteca pandas para criação do arquivo CSV
 import pandas as pd
 
